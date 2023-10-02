bf5381b47940b62a6864e263c0ee3bc28e49fc68
ahmadzen022 <141709205+ahmadzen022@users.noreply.github.com>
2023-10-02 13:12:45 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+
+function profile(namaDepan, namaBelakang, email) {
+    console.log("Nama : " + namaDepan + " "+ namaBelakang)
+    console.log("Email : "+ email)
+}
+
+profile("Ahmad","zeen N","zeen@gmail.com")
